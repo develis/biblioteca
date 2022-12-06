2448119fd4ae73fedf9879b44cd1c8f582a19ff1
Elis Alcantara <Elis.Alcantara-ext@ab-Inbev.com>
2022-12-06 15:44:21 -0300

    wip: emprestimo command

--- src/main/java/biblioteca/command/EmprestimoCommand.java
+++ src/main/java/biblioteca/command/EmprestimoCommand.java
@@ -1,34 +1,18 @@
 package biblioteca.command;
 
+import static java.lang.System.exit;
+
 import java.util.Date;
 
 import biblioteca.BibliotecaDAOImpl;
 import biblioteca.UsuarioDAOImpl;
 import biblioteca.model.Emprestimo;
 import biblioteca.model.Exemplar;
+import biblioteca.model.Hierarquia;
 import biblioteca.model.Livro;
 import biblioteca.model.Reserva;
 import biblioteca.model.Status;
 import biblioteca.model.Usuario;
-
-/*
-O sistema deve permitir o empréstimo de livros. Durante o empréstimo, o usuário informará o comando “emp” seguido do código do usuário e do código do
-livro, separados por espaço em branco. Ex.: “emp 123 100”. Caso o usuário tenha uma reserva feita previamente por ele para o dado livro, a reserva deve
-ser excluída e o empréstimo efetivado. Ao final do procedimento o sistema deve emitir uma mensagem de sucesso ou insucesso, que mencione o nome do
-usuário e o título do livro. Se for uma mensagem de insucesso, ela deve também mencionar o motivo do insucesso.
-O empréstimo do livro só será concretizado para um aluno de graduação ou um aluno de pós-graduação se: (i) houver a disponibilidade de algum
-exemplar daquele livro na biblioteca; (ii) o usuário não estiver “devedor” de um livro em atraso; (iii) forem obedecidas as regras específicas
-daquele tipo de usuário no que se refere à quantidade máxima de empréstimos, de acordo com a Tabela 2; (iv) a quantidade de reservas existentes do
-livro for menor do que a quantidade de exemplares disponíveis, caso o usuário não tenha reserva para
-ele; (v) a quantidade de reservas for maior ou igual a de exemplares, mas uma das reservas é do usuário; e (vi) o usuário não tiver nenhum
-empréstimo em curso de um exemplar daquele mesmo livro.
-O empréstimo do livro só será concretizado para um professor se: (i) houver a disponibilidade de algum exemplar daquele livro na biblioteca; e (ii)
-o usuário não estiver “devedor” de um livro em atraso.
-Note que os professores não tem empréstimo negado caso haja reservas para aquele livro e não tem limite da quantidade de livros que pode pegar
-emprestado.
-É sabido que nesse tipo de domínio essas regras estão sujeitas a frequentes mudanças. Além disso, podem surgir novos tipos de usuário para os
-quais as regras de empréstimo sejam diferentes das já existentes para professor e alunos de graduação e de pós.
-* */
 
 public class EmprestimoCommand extends Command{
     public EmprestimoCommand(final BibliotecaDAOImpl bibliotecaDAO, final UsuarioDAOImpl usuarioDAO, final String comando) {
@@ -43,6 +27,25 @@ public class EmprestimoCommand extends Command{
 
         Usuario usuario = usuarioDAO.findById(id_usuario);
         Livro livro = bibliotecaDAO.findLivroById(id_livro);
+        boolean requisitos = requisitosAlunos(usuario, livro);
+
+        if (livro.getExemplaresDisponiveis().isEmpty()) {
+            System.out.println("- Operação não sucedeu.");
+            System.out.println("* Não há exemplares disponíveis.");
+            exit(1);
+            return false;
+        }
+
+        if ((usuario.getHierarquia() == Hierarquia.GRAD || usuario.getHierarquia() == Hierarquia.POS_GRAD) && !requisitos) {
+            System.out.println("- Operação não sucedeu.");
+            exit(1);
+            return false;
+        }
+
+        if (usuario.getHierarquia() == Hierarquia.PROFESSOR && temEmprestimoAtivo(usuario, livro)) {
+            exit(1);
+            return false;
+        }
 
         if (!livro.getExemplaresDisponiveis().isEmpty()) {
             exemplar = finalizarReservaUsuario(usuario, id_livro);
@@ -92,10 +95,58 @@ public class EmprestimoCommand extends Command{
         return new Exemplar();
     }
 
-    public static void main(String[] args) {
-        UsuarioDAOImpl dao = new UsuarioDAOImpl();
-        EmprestimoCommand command = new EmprestimoCommand(new BibliotecaDAOImpl(), dao, "emp 1 101");
-        command.execute();
-        System.out.println(dao.findById(1L).getEmprestimos().get(1).getExemplar().getTitulo());
+    private boolean requisitosAlunos(Usuario usuario, Livro livro) {
+        if (
+                livrosEmAtraso(usuario) ||
+                        limiteEmprestimos(usuario) ||
+                        temEmprestimoAtivo(usuario, livro) ||
+                        !(!alunoTemReserva(usuario, livro) && livro.getReservas().size() < livro.getExemplaresDisponiveis().size()) ||
+                        !(alunoTemReserva(usuario, livro) && livro.getReservas().size() >= livro.getExemplaresDisponiveis().size())
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean alunoTemReserva(Usuario usuario, Livro livro) {
+        for (Reserva reserva: usuario.getReservas()) {
+            if (reserva.getExemplar().getId_livro().intValue() == livro.getId()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean temEmprestimoAtivo(Usuario usuario, Livro livro) {
+        for (Emprestimo emprestimo: usuario.getEmprestimos()) {
+            if (emprestimo.getExemplar().getId_livro() == livro.getId()) {
+                System.out.println("* Usuário possui empréstimo ativo");
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean limiteEmprestimos(Usuario usuario) {
+        if (usuario.getEmprestimos().size() >= usuario.getLimite_livros() && usuario.getLimite_livros() != 0) {
+            System.out.println("* Usuário atingiu o limite de empréstimos");
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean livrosEmAtraso(Usuario usuario) {
+        for (Emprestimo emprestimo: usuario.getEmprestimos()) {
+            if (new Date().after(emprestimo.getData_devolucao()) && emprestimo.getStatus() == Status.EM_ANDAMENTO) {
+                System.out.println("* Usuário possui empréstimos em atraso");
+                return true;
+            }
+        }
+
+        return false;
     }
 }
